06811b87caad73400e354cf8066f9b782bd0e048
Josep Quintana <josep.quintana.44@gmail.com>
2022-10-12 12:21:53 +0200

    blockchain files

--- src/blockchain/truffle-config.js
+++ src/blockchain/truffle-config.js
@@ -28,6 +28,16 @@ module.exports = {
       network_id: "*" // Any network (default: none)
     }
   },
+  
+  // An additional network, but with some advanced options…
+  // advanced: {
+  //   port: 8777,             // Custom port
+  //   network_id: 1342,       // Custom network
+  //   gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+  //   gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+  //   from: <address>,        // Account to send transactions from (default: accounts[0])
+  //   websocket: true         // Enable EventEmitter interface for web3 (default: false)
+  // },
 
   // Smart Contracts directory
   contracts_directory: './contracts/',
